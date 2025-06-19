5ee9e783c5dabd53d67eed4e0e1df2dff1cf715a
JorenVsyntra <vandervelden.joren@gmail.com>
2025-06-19 16:30:45 +0200

    Add dashboard page and related widgets for order and stock overview

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+use App\Filament\Resources\DashboardResource\Widgets\LeveringenOverview; 
+use App\Filament\Resources\DashboardResource\Widgets\VoorraadOverview;
+use App\Filament\Resources\DashboardResource\Widgets\LeveringenChart;
+use App\Filament\Resources\DashboardResource\Widgets\VoorraadChart;
+
+class Dashboard extends Page
+{
+    protected static string $view = 'filament.pages.dashboard';
+
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            LeveringenOverview::class,
+            LeveringenChart::class,
+            VoorraadOverview::class,
+            VoorraadChart::class
+        ];
+    }
+}
---
+++ app/Filament/Resources/DashboardResource/Widgets/LeveringenOverview.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources\DashboardResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\User;
+use App\Models\OrderItem;
+use App\Models\Delivery;
+use App\Models\DeliveryItem;
+use App\Models\Event;
+use App\Models\Item;
+use App\Models\Location;
+use App\Models\Order;
+use App\Models\StockTransaction;
+use App\Models\Supplier;
+use App\Models\Category;
+use App\Models\Unit;
+use App\Models\Vendor;
+use App\Models\VendorItemStock;
+
+
+
+
+class LeveringenOverview extends BaseWidget
+{
+    protected ?string $heading = 'Leveringen';
+
+
+    protected static ?int $sort = 1;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $recordUrlAttribute = 'slug';
+
+    protected static ?string $recordUrlRoute = 'filament.resources.dashboard-resource.show';
+
+    protected static ?string $recordUrlRouteParameter = 'dashboardResource';
+
+    protected function getStats(): array
+    {
+        $pendingOrders = Order::where('status', 'pending')->count();
+        $totalShippedOrders = Order::where('status', 'sent')->count();
+        $totalDeliveredOrders = Order::where('status', 'delivered')->count();
+
+
+
+        return [
+            Stat::make('Niet-goedgekeurde bestellingen', $pendingOrders)
+                ->color('warning')
+                ->description('Bestellingen die nog niet zijn goedgekeurd')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer',
+                    'x-on:click' => "window.location.href='/orders'",
+                ]),
+            
+            Stat::make('Verstuurde bestellingen', $totalShippedOrders)
+                ->color('primary')
+                ->description('Bestellingen die al zijn verzonden')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer',
+                    'x-on:click' => "window.location.href='/orders'",
+                ]),
+            
+
+            Stat::make('Geleverde bestellingen', $totalDeliveredOrders)
+                ->color('success')
+                ->description('Bestellingen die al zijn geleverd')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer',
+                    'x-on:click' => "window.location.href='/deliveries'",
+                ]),
+
+        ];
+    }
+}
